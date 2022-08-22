29da19707cf9cbf9361f348d7f5500a3b3b27785
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2022-08-22 10:58:52 -0500

    finished required task

--- app/src/main/java/com/example/calculator_app/MainActivity.java
+++ app/src/main/java/com/example/calculator_app/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculator_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void findSum(View view){
+        EditText firstNumber = findViewById(R.id.firstValue);
+        EditText secondNumber = findViewById(R.id.secondValue);
+        TextView output = findViewById(R.id.output);
+
+        int num1 = Integer.parseInt(firstNumber.getText().toString());
+        int num2 = Integer.parseInt(secondNumber.getText().toString());
+
+        int sum = num1 + num2;
+        output.setText(""+sum);
+    }
+
+    public void findDifference(View view){
+        EditText firstNumber = findViewById(R.id.firstValue);
+        EditText secondNumber = findViewById(R.id.secondValue);
+        TextView output = findViewById(R.id.output);
+
+        int num1 = Integer.parseInt(firstNumber.getText().toString());
+        int num2 = Integer.parseInt(secondNumber.getText().toString());
+
+        int Difference = num1 - num2;
+        output.setText(""+Difference);
+    }
+
+    public void findProduct(View view){
+        EditText firstNumber = findViewById(R.id.firstValue);
+        EditText secondNumber = findViewById(R.id.secondValue);
+        TextView output = findViewById(R.id.output);
+
+        int num1 = Integer.parseInt(firstNumber.getText().toString());
+        int num2 = Integer.parseInt(secondNumber.getText().toString());
+
+        int Product = num1 * num2;
+        output.setText(""+Product);
+    }
+
+    public void findQuotient(View view){
+        EditText firstNumber = findViewById(R.id.firstValue);
+        EditText secondNumber = findViewById(R.id.secondValue);
+        TextView output = findViewById(R.id.output);
+
+        int num1 = Integer.parseInt(firstNumber.getText().toString());
+        int num2 = Integer.parseInt(secondNumber.getText().toString());
+
+        int Quotient = num1 / num2;
+        output.setText(""+Quotient);
     }
 }
